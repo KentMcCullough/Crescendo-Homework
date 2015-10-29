5c6ad469b4def139c6f302d8a30c8db60bf1d2ed
KentMcCullough <kent@kentmccullough.com>
2015-10-28 23:22:14 -0500

    Initial Commit

---
+++ Crescendo Homework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crescendo Homework
+//
+//  Created by Kent McCullough on 10/28/15.
+//  Copyright © 2015 Kent McCullough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
